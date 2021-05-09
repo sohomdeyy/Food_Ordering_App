21234bbeddd96e169cf29249b4b0aa7ea3eea9db
sohomdeyy <80444727+sohomde@users.noreply.github.com>
2021-05-09 13:28:54 +0530

    second page created

--- app/src/main/java/com/example/tomato/MainActivity.java
+++ app/src/main/java/com/example/tomato/MainActivity.java
@@ -17,6 +17,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        //testing
     imageView=findViewById(R.id.imageView);
     textView=findViewById(R.id.textView7);
 
